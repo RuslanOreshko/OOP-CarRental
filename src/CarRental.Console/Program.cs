085a3b158b7f1861a908545472b769d1df0a2ce9
RuslanOreshko <3na3drocher@gmail.com>
2026-05-24 21:03:58 +0300

    feat(console): add a simple console ui for present lab34

--- src/CarRental.Console/Program.cs
+++ src/CarRental.Console/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CarRental.Application.DTOs;
+using CarRental.Infrastructure.Repositories;
+using CarRental.Application.Services;
+
+var carRepository = new InMemoryCarRepository();
+
+var rentalService = new RentalCarService(carRepository);
+
+Console.WriteLine("Car Rental System");
+Console.WriteLine();
+
+var cars = carRepository.GetAll().ToList();
+
+Console.WriteLine("Available cars:");
+
+foreach(var car in cars)
+{
+    Console.WriteLine(
+        $"{car.Id} | {car.Brand} | {car.Model} | ${car.PricePerDay}/day | Available: {car.IsAvaible}"
+    );
+}
+
+Console.WriteLine();
+Console.Write("Enter car id: ");
+
+var carIdInput = Console.ReadLine();
+
+if(!Guid.TryParse(carIdInput, out var carId))
+{
+    Console.WriteLine("Invalid car id.");
+    return;
+}
+
+Console.Write("Enter rental days: ");
+
+if (!int.TryParse(Console.ReadLine(), out var days))
+{
+    Console.WriteLine("Invalid days.");
+    return;
+}
+
+var request = new RentalCarRequest
+{
+  CarId = carId,
+  CustomerId = Guid.NewGuid(),
+  Days = days
+};
+
+try
+{
+    var response = rentalService.RentalCar(request);
+
+    Console.WriteLine();
+    Console.WriteLine(response.Message);
+    Console.WriteLine($"Rental Id: {response.RentalId}");
+    Console.WriteLine($"Total Price: ${response.TotalPrice}");
+}
+catch (Exception ex)
+{
+   Console.WriteLine($"Error: {ex.Message}"); 
+}
